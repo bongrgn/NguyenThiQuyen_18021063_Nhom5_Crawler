df92532608c7901151b03b57fa4f3d083d990381
Nguyễn Thị Quyên <58633199+bongrgn@users.noreply.github.com>
2020-08-08 18:51:31 +0700

    Add files via upload

---
+++ Crawler.py
@@ -0,0 +1,42 @@
+import json
+import scrapy
+
+
+class VnexpressSpider(scrapy.Spider):
+    name = 'vnexpress3'
+    allowed_domains = ['vnexpress.net']
+    start_urls = ['https://vnexpress.net']
+    CRAWLED_COUNT = 0
+
+    def parse(self, response):
+        if response.status == 200 and response.css('meta[name="tt_page_type"]::attr("content")').get() == 'article':
+            print('Crawling from:', response.url)
+            data = {
+                'link': response.url,
+                'title': response.css('h1.title-detail::text').get(),
+                'description': response.css('p.description::text').get(),
+
+                'content': '\n'.join([
+                    ''.join(c.css('*::text').getall())
+                        for c in response.css('article.fck_detail p.Normal')
+                ]),
+
+                'category': response.css('meta[itemprop="articleSection"]::attr("content")').get(),
+                'pub_date': float(response.css('meta[name="its_publication"]::attr("content")').get()),
+                'keywords': [
+                    k.strip() for k in response.css('meta[name="keywords"]::attr("content")').get().split(',')
+                ],
+                'tags': [
+                    k.strip() for k in response.css('meta[name="its_tag"]::attr("content")').get().split(',')
+                ],
+            }
+
+            with open("C:/Users/DELL/PycharmProjects/Crawl1/tutorial/Output/trang1.txt", 'a', encoding='utf8') as f:
+                f.write(json.dumps(data, ensure_ascii=False))
+                f.write('\n')
+                self.CRAWLED_COUNT += 1
+                self.crawler.stats.set_value('CRAWLED_COUNT', self.CRAWLED_COUNT)
+                print('SUCCESS:', response.url)
+                print('COUNT' , self.CRAWLED_COUNT)
+
+        yield from response.follow_all(css='a[href^="https://vnexpress.net"]::attr(href), a[href^="/"]::attr(href)', callback=self.parse)
